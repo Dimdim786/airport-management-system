4cb37b01244e98074ddf460f807b743790a43f90
ZQR0 <yarikrech@gmail.com>
2025-12-01 12:07:19 +0300

    Пример репозитория для пользователей

---
+++ src/main/java/rut/miit/airportweb/dao/repository/UserRepository.java
@@ -0,0 +1,29 @@
+package rut.miit.airportweb.dao.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import rut.miit.airportweb.dao.entity.UserEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+
+/**
+ * Пример репозитория для сущности пользователя.
+ * В нём представлены примерные методы, возможно потом будем добавлять другие методы.
+ * Как видно, можно кастомные запросы писать под @Query, но там используется не нативный SQL, а
+ * HQL (Hibernate Query Language)
+ * В целом по названию методов думаю понятно что они делают
+ * */
+@Repository
+public interface UserRepository {
+
+    Optional<UserEntity> findByUsername(String username);
+
+    @Query("SELECT DISTINCT u FROM user_entity u JOIN FETCH u.passenger")
+    List<UserEntity> findAllOptimized();
+
+    @Query("SELECT u FROM user_entity u WHERE u.firstName=:firstName AND u.lastName=:lastName")
+    Optional<UserEntity> findByFirstNameAndLastName(String firstName, String lastName);
+
+}
